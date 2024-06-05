62fe6cae5531b9f544cc021249021449dbe4c18d
rogercf17 <rogercf16@gmail.com>
2024-06-05 20:19:25 -0300

    Fix

---
+++ js/scriptContato.js
@@ -0,0 +1,38 @@
+// VALIDAÇÃO DO FORMULÁRIO DE CONTATO
+const btnFormContato = document.querySelector('#btn-contato');
+document.querySelector('#btn-contato').addEventListener('click', function(Evento) {
+    Evento.preventDefault();
+    const nome = document.querySelector("#nome").value.trim();    
+    const email = document.querySelector("#email").value.trim();    
+    const telefone = document.querySelector("#telefone").value.trim();
+    const mensagem = document.querySelector("#mensagem").value.trim();
+    let validacao = true;
+    let mensagemErro = [];
+    if(!nome || !email || !telefone || !mensagem) {
+        mensagemErro.push("Todos os campos devem ser preenchidos.");
+        validacao = false;
+    }
+    if(nome.length < 3) {
+        mensagemErro.push("O nome deve ter pelo menos 3 letras.");
+        validacao = false;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)) {
+        mensagemErro.push("Por favor, insira um email inválido.");
+        validacao = false;
+    }
+    const telefoneRegex = /^[0-9]{10,}$/;
+    if (!telefoneRegex.test(telefone)) {
+        mensagemErro.push("Por favor, insira um telefone válido com pelo menos 10 digítos.")
+        validacao = false;
+    }
+    if (!validacao) {
+        alert(mensagemErro.join("\n"));
+    } else {
+        alert("Formulário enviado com sucesso!");
+        document.querySelector("#nome").value = "";
+        document.querySelector("#email").value = "";
+        document.querySelector("#telefone").value = "";
+        document.querySelector("#mensagem").value = "";
+    }
+});
